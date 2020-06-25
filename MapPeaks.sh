a2063783170ce3eb5d2c1a99602cdf9120ec3f97
sknaack <sara.a.knaack@gmail.com>
2020-06-25 15:52:28 -0500

    Make sure example files work.
    Commenting out lines for generating the .gff and the input .bed format file.

--- MapPeaks.sh
+++ MapPeaks.sh
@@ -1,9 +1,9 @@
-awk '$3=="gene"{print}' example/MtrunA17r5.0-ANR-EGN-r1.6.gff3 | sed 's/;/\t/g;s/=/\t/g' | cut -d $'\t' -f1-8,12 > example/Medicago_v5_simplified.gff
-
-cut -f1 example/Medicago_v5_simplified.gff | sort -u | awk '{printf("%s\t%s\n",$1,$1)}' > example/medicago_v5_chr_name_mapping.txt
+#awk '$3=="gene"{print}' example/MtrunA17r5.0-ANR-EGN-r1.6.gff3 | sed 's/;/\t/g;s/=/\t/g' | cut -d $'\t' -f1-8,12 > example/Medicago_v5_simplified.gff
+#cut -f1 example/Medicago_v5_simplified.gff | sort -u | awk '{printf("%s\t%s\n",$1,$1)}' > example/medicago_v5_chr_name_mapping.txt
 
 #format the bed file for input
-awk '{printf("%s\t%i\t%i\tpeak%i\n",$1,$2,$3,NR)}' example/Control_merged_peaks.bed > example/tmp.bed
+printf "Chromosome\tStart\tEnd\tName\n" > example/tmp.bed
+awk '{printf("%s\t%i\t%i\tpeak%i\n",$1,$2,$3,NR)}' example/Control_merged_peaks.bed >> example/tmp.bed
 
 ./MapPeaksToGenes example/Medicago_v5_simplified.gff example/medicago_v5_chr_name_mapping.txt example/tmp.bed double example/test_output.txt 10000 1000 example/test_no_mapping.txt
 
